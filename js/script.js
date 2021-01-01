f79c94c611edc4be455f0791e16dd6c6ac37f696
ab-101 <56578530+ab-101@users.noreply.github.com>
2021-01-01 11:01:12 +0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+var medicine = ["Panadol","Augmentin","Ibuprofen","Acetaminophen","Losartan","Metformin","Amitriptyline"];
+
 function revealMessage() {
 	document.getElementById("hiddenMessage").style.display = 'block';
 }
@@ -9,9 +11,15 @@ function countDown(){
 	
 	if (currentVal>0){
 		newval = currentVal - 1;
+		
 	}
+
 	
 	document.getElementById("countDownButton").innerHTML = newval;
+	
+	var position = Math.floor(Math.random() * 7);      // returns a random integer from 0 to 2
+	
+	document.getElementById("medicinename").innerHTML = medicine[position];		// get random medicine name based on above generated random variable
 	
 }
 	
